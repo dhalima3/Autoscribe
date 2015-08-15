7e02295bac07560e30ae51703d2be5bc744e97e0
dhalima3 <dhalima@gatech.edu>
2015-08-15 00:33:57 -0700

    google suggestions script

---
+++ googleSuggestions.py
@@ -0,0 +1,16 @@
+import requests
+from xml.etree import ElementTree
+
+wordFile = open("words.txt")
+wordList = wordFile.readlines()
+baseUrl = "http://clients1.google.com/complete/search?hl=en&output=toolbar&q="
+
+for word in wordList:
+    word = word.strip()
+    response = requests.get(baseUrl + word)
+    root = ElementTree.fromstring(response.content)
+    fileName = word + ".txt"
+    resultsFile = open(fileName, "w")
+    for suggestion in root.iter('suggestion'):
+        resultsFile.write(suggestion.attrib['data'] + "\n")
+    resultsFile.close()
